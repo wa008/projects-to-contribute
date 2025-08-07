1eb14711aa3c3ad1d7ab49ab2b9971a08b20a8b1
wa008 <hzp1024@qq.com>
2025-08-07 12:35:25 +0800

    Update fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -34,7 +34,7 @@ TOPIC_MAP = {
 
 ACRONYMS = ['AI', 'ML', 'NLP', 'API', 'CLI', 'CI-CD', 'SQL']
 PROGRESS_FILE = 'progress.json'
-MAX_REQUESTS = 500
+MAX_REQUESTS = 12000
 
 class GitHubAPI:
     """
